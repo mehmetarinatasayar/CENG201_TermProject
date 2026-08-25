c2f88bbe63396ff0123cb57b0cfaf4b184708147
Mehmet Arın Atasayar <s210444088@stu.thk.edu.tr>
2026-08-25 19:55:53 +0300

    add explicit WP7 integration demo entry point

---
+++ src/WP7Demo.java
@@ -0,0 +1,9 @@
+public class WP7Demo {
+
+    public static void main(String[] args) {
+        ExamGateEngine engine =
+                new ExamGateEngine();
+
+        engine.run(20260725L);
+    }
+}
